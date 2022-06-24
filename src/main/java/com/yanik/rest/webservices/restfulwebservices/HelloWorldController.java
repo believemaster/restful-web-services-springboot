c84f06f38a963242f9faa4bf6ff465d2fa007b27
believemaster <believemasters@gmail.com>
2022-06-24 20:27:30 +0530

    Allowing cross origin request form spring api to react frontend

--- src/main/java/com/yanik/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/yanik/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.yanik.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller
 @RestController
+@CrossOrigin(origins="http://localhost:4200")		// allowing react-app to request from the url so to make cross origin set the origin
 public class HelloWorldController {
 	// GET
 	// URI - /hello-world
@@ -13,19 +15,19 @@ public class HelloWorldController {
 	
 //	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
 //	use above annotation or easy one below
-	@GetMapping(path="/hello-world")
+	@GetMapping(path="/api/hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
 	
 	// hello-world-bean
-	@GetMapping(path="/hello-world-bean")
+	@GetMapping(path="/api/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
 	}
 	
 	// hello-world/path-variable/yanik
-	@GetMapping(path="/hello-world/path-variable/{name}")
+	@GetMapping(path="/api/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
